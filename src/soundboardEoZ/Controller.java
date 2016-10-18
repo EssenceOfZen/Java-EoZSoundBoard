67058f3d03cf68217b263e0115037bb49987701c
Zane Blalock <ZaneBlalock@EssenceOfZen.org>
2016-10-18 16:52:03 -0400

    Dynamic Button Loading now functional.

--- src/soundboardEoZ/Controller.java
+++ src/soundboardEoZ/Controller.java
@@ -1,4 +1,33 @@
 package soundboardEoZ;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    int index = 1;
+    int target = 10;
+
+    @FXML
+    GridPane sound_bites_grid;
+
+    public void initialize(){ /*This is called when the program runs*/
+        System.out.println("Hey");
+        while(index <= target){
+            Button sound_button = new Button("Button_" + index);
+            sound_bites_grid.add(sound_button,index,1);
+            index++;
+        }
+
+    }
+
+    void test(){
+        System.out.println("Testing");
+    }
+
+    void addButton(){
+        Button sound_button = new Button("Button_" + index);
+        sound_bites_grid.add(sound_button, index,2);
+    }
+
 }
--- src/soundboardEoZ/Main.java
+++ src/soundboardEoZ/Main.java
@@ -10,34 +10,18 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @FXML private GridPane button_grid;
-    int sound_index = 0;
-    private Sound sounds[] = {new Sound("Clap"), new Sound("Shouting", "/music"), new Sound()};
+    //private Sound sounds[] = {new Sound("Clap"), new Sound("Shouting", "/music"), new Sound()};
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception{ /*Main class main method that starts the gui*/
+    /*We basically state that we load the fxml file as our resources and set the stage for the window*/
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-//        Sound sounds[]; // We create an array that will hold our sounds objects
 
-
-        for (Sound sound : sounds) {
-            // Add buttons to the gui!
-
-
-
-        }
     }
-
-    private void addButton(){
-        final Button temporary = new Button()
-    }
-
-
 
     public static void main(String[] args) {
         launch(args);
