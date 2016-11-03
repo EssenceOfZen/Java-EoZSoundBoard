ebe7574f4c4c84b1aaa78143f54f688e55669ea2
Zane Blalock <ZaneBlalock@EssenceOfZen.org>
2016-11-03 18:34:24 -0400

    few changes to classes for dynamic loading.

--- src/soundboardEoZ/Controller.java
+++ src/soundboardEoZ/Controller.java
@@ -4,6 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Controller {
     int index = 1;
     int target = 10;
@@ -13,6 +18,12 @@ public class Controller {
 
     public void initialize(){ /*This is called when the program runs*/
         System.out.println("Hey");
+
+        String location = System.getProperty("user.dir"); // SystemGrabs the project directory
+        int length = getNumbersOfFiles(location + "/settings/");
+        System.out.println("Running some things: " + location);
+        System.out.println("Number of files in Settings: " + length);
+
         while(index <= target){
             Button sound_button = new Button("Button_" + index);
             sound_bites_grid.add(sound_button,index,1);
@@ -21,8 +32,10 @@ public class Controller {
 
     }
 
-    void test(){
-        System.out.println("Testing");
+    int getNumbersOfFiles(String directory){
+        int length = new File(directory).listFiles().length;
+
+        return length;
     }
 
     void addButton(){
